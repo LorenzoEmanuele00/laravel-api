3a45e65944f475ba4ee7c9e1cccf2a7816c8e28a
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2024-02-05 23:00:28 +0100

    add pagination on api part

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::paginate(8);
 
         return response()->json([
             'result' => $projects,
